ac3d5d8e6b890717a1f26bab5e14e12a8496fda7
Steve Galbraith <steve@stevegalbraith.com>
2016-09-02 14:53:32 -0600

    Add seeds for user categories and quesitons #12

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+STARTING_CATEGORIES = %w(fun love/relationships spiritual family friends work)
+
+STARTING_CATEGORIES.each do |category_name|
+  category = Category.create(name: category_name.split('/').map(&:capitalize).join('/'))
+  3.times do
+    Question.create(text: "How do you feel about #{Faker::Company.catch_phrase.downcase.pluralize}?", category: category)
+  end
+end
+
+user = User.create({
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: 'first.user@example.com',
+  password: 'Pa55w0rd',
+  password_confirmation: 'Pa55w0rd'
+})
